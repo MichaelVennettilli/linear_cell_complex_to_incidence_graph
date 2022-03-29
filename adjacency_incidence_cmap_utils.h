fb4e8e26ab1e64c52b1b64851260eb89e2116dfa
Michael Vennettilli <mikepvenn@gmail.com>
2022-03-29 12:29:06 +0200

    further modularized the code to eliminate for loops that were repeated several times. implemented adjacency checking for vertices

--- adjacency_incidence_cmap_utils.h
+++ adjacency_incidence_cmap_utils.h
@@ -40,103 +40,89 @@ typedef LCC_3::typename CMap::Dart_handle           typename CMap::Dart_handle_3
 typedef LCC_3::Vertex_attribute_handle           Vertex_handle_3;
 
 // Inverse function for indices
-unsigned int inv(unsigned int i){
+inline unsigned int inv(unsigned int i){
   return (i>1) ? i : (1-i);
+}
+
+// Mark all darts in c_i(dh)
+template<typename CMap>
+void mark_darts_in_cell(CMap &cm, unsigned int i, typename CMap::Dart_handle &dh, typename CMap::size_type ma){
+  for(typename CMap::Dart_of_cell_range<i>::iterator
+    dart_it=cm.darts_of_cell<i>(dh).begin(),
+    dart_it_end=cm.darts_of_cell<i>(dh).end();
+    dart_it!=dart_it_end; ++dart_it){
+    cm.mark(dart_it, ma);
+  }
+  return;
+}
+
+// Unmark all darts in c_i(dh)
+template<typename CMap>
+void unmark_darts_in_cell(CMap &cm, unsigned int i, typename CMap::Dart_handle &dh, typename CMap::size_type ma){
+  for(typename CMap::Dart_of_cell_range<i>::iterator
+    dart_it=cm.darts_of_cell<i>(dh).begin(),
+    dart_it_end=cm.darts_of_cell<i>(dh).end();
+    dart_it!=dart_it_end; ++dart_it){
+    cm.unmark(dart_it, ma);
+  }
+  return;
+}
+
+// Check if a dart in c_i(dh) has already been marked
+template<typename CMap>
+bool is_dart_in_cell_marked(CMap &cm, unsigned int i, typename CMap::Dart_handle &dh, typename CMap::size_type ma){
+  bool result = false;
+  typename CMap::Dart_of_cell_range<i>::iterator dart_it=cm.darts_of_cell<i>(dh).begin();
+  typename CMap::Dart_of_cell_range<i>::iterator dart_it_end=cm.darts_of_cell<i>(dh).end();
+  while((!result)&&(dart_it!=dart_it_end)){
+    if(cm.is_marked(dart_it,ma)) result = true;
+    ++dart_it;
+  }
+  return result;
 }
 
 // Returns true if the i-cell corresponding to dh_i is incident to the
 // j-cell corresponding to dh_j.
-template<typename CMap, unsigned int i, unsigned int j>
-bool check_incident_pair(CMap &cm, typename CMap::Dart_handle &dh_i, typename CMap::Dart_handle &dh_j){
-  bool result = false;
+template<typename CMap>
+bool check_incident_pair(CMap &cm, unsigned int i, typename CMap::Dart_handle &dh_i,
+  unsigned int j, typename CMap::Dart_handle &dh_j){
   typename CMap::size_type ma = cm.get_new_mark();
-  // Mark everything in the i-cell belonging to c_i(dh_i)
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    i_cell_it=cm.darts_of_cell<i>(dh_i).begin(),
-    i_cell_end=cm.darts_of_cell<i>(dh_i).end();
-    i_cell_it!=i_cell_end; ++i_cell_it){
-    cm.mark(i_cell_it, ma);
-  }
-  // Loop over darts in c_j(dh_j) until you reach the end or hit something marked
-  typename CMap::Dart_of_cell_range<j>::iterator j_cell_it=cm.darts_of_cell<j>(dh_j).begin();
-  typename CMap::Dart_of_cell_range<j>::iterator j_cell_end=cm.darts_of_cell<j>(dh_j).end();
-  while((!result)&&(j_cell_it!=j_cell_end)){
-    if(cm.is_marked(j_cell_it,ma)) result = true;
-    ++j_cell_it;
-  }
-  // Unmark everything and free the mark
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    i_cell_it=cm.darts_of_cell<i>(dh_i).begin(),
-    i_cell_end=cm.darts_of_cell<i>(dh_i).end();
-    i_cell_it!=i_cell_end; ++i_cell_it){
-    cm.unmark(i_cell_it, ma);
-  }
+  mark_darts_in_cell(cm, i, dh_i, ma);
+  bool result = is_dart_in_cell_marked(cm, j, dh_j, ma);
+  unmark_darts_in_cell(cm, i, dh_i, ma);
   cm.free_mark(ma);
   return result;
 }
 
 // Returns true if c_i(dh_1) is adjacent to c_i(dh_2). Work with the case i > 0 first.
-template<typename CMap, unsigned int i>
-bool check_adjacent_pair(CMap &cm, typename CMap::Dart_handle &dh_1, typename CMap::Dart_handle &dh_2){
+template<typename CMap>
+bool check_adjacent_pair(CMap &cm, unsigned int i, typename CMap::Dart_handle &dh_1, typename CMap::Dart_handle &dh_2){
   bool result = false;
   typename CMap::size_type ma = cm.get_new_mark();
-  // Mark everything in c_i(dh_1)
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    i_cell_it=cm.darts_of_cell<i>(dh_1).begin(),
-    i_cell_end=cm.darts_of_cell<i>(dh_1).end();
-    i_cell_it!=i_cell_end; ++i_cell_it){
-    cm.mark(i_cell_it, ma);
-  }
-  // Loop over things in c_i(dh_2) and see if applying Beta_i gets you
-  // to something that has already been hit.
-  typename CMap::Dart_of_cell_range<i>::iterator i_cell_it=cm.darts_of_cell<i>(dh_2).begin();
-  typename CMap::Dart_of_cell_range<i>::iterator i_cell_end=cm.darts_of_cell<i>(dh_2).end();
-  while((!result)&&(i_cell_it!=i_cell_end)){
-    if(cm.is_marked(cm.beta(i_cell_it,i),ma)) result = true;
-    if(cm.is_marked(cm.beta(i_cell_it, inv(i)),ma)) result = true;
-    ++j_cell_it;
-  }
-  // Unmark everything and free the mark
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    i_cell_it=cm.darts_of_cell<i>(dh_1).begin(),
-    i_cell_end=cm.darts_of_cell<i>(dh_1).end();
-    i_cell_it!=i_cell_end; ++i_cell_it){
-    cm.unmark(i_cell_it, ma);
-  }
-  cm.free_mark(ma);
-  return result;
-}
+  mark_darts_in_cell(cm, i, dh_1, ma);
 
-// Returns true if c_0(dh_1) is adjacent to c_0(dh_2)
-template<typename CMap, unsigned int i>
-bool check_adjacent_pair<CMap, 0>(CMap &cm, typename CMap::Dart_handle &dh_1, typename CMap::Dart_handle &dh_2){
-  bool result = false;
-  typename CMap::size_type ma = cm.get_new_mark();
-  int dim = cm.dimension;
-  // Mark everything in c_i(dh_1)
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    dart_it=cm.darts_of_cell<0>(dh_1).begin(),
-    dart_it_end=cm.darts_of_cell<0>(dh_1).end();
-    dart_it!=dart_it_end; ++dart_it){
-    cm.mark(dart_it, ma);
-  }
-  // Loop over things in c_i(dh_2) and see if applying Beta_i gets you
-  // to something that has already been hit.
-  typename CMap::Dart_of_cell_range<i>::iterator dart_it=cm.darts_of_cell<0>(dh_2).begin();
-  typename CMap::Dart_of_cell_range<i>::iterator dart_it_end=cm.darts_of_cell<0>(dh_2).end();
-  while((!result)&&(dart_it!=dart_it_end)){
-    for(int i=0; i<= dim; i++){
-      if(cm.is_marked(cm.beta(dart_it,i),ma)) result = true;
+  typename CMap::Dart_of_cell_range<i>::iterator dart_it=cm.darts_of_cell<i>(dh_2).begin();
+  typename CMap::Dart_of_cell_range<i>::iterator dart_it_end=cm.darts_of_cell<i>(dh_2).end();
+  if(i==0){
+    unsigned int max_dimension = cm.dimension;
+    while((!result)&&(dart_it!=dart_it_end)){
+      for(unsigned int dim_it = 0; dim_it <= max_dimension; dim_it++){
+        if(cm.is_marked(cm.beta(dart_it,i),ma)) result = true;
+      }
+      ++dart_it;
     }
-    ++dart_it;
+  } else {
+    // Loop over things in c_i(dh_2) and see if applying Beta_i  of Beta_{inv(i)}
+    // gets you to something that has already been hit.
+    while((!result)&&(dart_it!=dart_it_end)){
+      if(cm.is_marked(cm.beta(dart_it,i),ma)) result = true;
+      if(cm.is_marked(cm.beta(dart_it, inv(i)),ma)) result = true;
+      ++dart_it;
+    }
   }
   // Unmark everything and free the mark
-  for(typename CMap::Dart_of_cell_range<i>::iterator
-    dart_it=cm.darts_of_cell<0>(dh_1).begin(),
-    dart_it_end=cm.darts_of_cell<0>(dh_1).end();
-    dart_it!=dart_it_end; ++dart_it){
-    cm.unmark(dart_it, ma);
-  }
+  unmark_darts_in_cell(cm, i, dh_1, ma);
+
   cm.free_mark(ma);
   return result;
 }
