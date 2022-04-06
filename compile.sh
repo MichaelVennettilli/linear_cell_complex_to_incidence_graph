3b039bd9ecfa452c02de060be8c43d80ab50a345
Michael Vennettilli <mikepvenn@gmail.com>
2022-04-06 16:15:03 +0200

    implemented computing the incidence graph with variadic templates, lambdas, and unfolding. the code compiles, but gives a runtime error. may be connected to problems with auto and type inference in the incidence and adjacency utils header.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cgal_create_CMakeLists -c Qt5
+sed -i '1 s/^/set(CMAKE_CXX_STANDARD 17)\n/' CMakeLists.txt
+#echo 'target_compile_features("main" PRIVATE cxx_std_17)' >>  CMakeLists.txt
 if [ ! -d build ]; then
 	echo DNE
 	mkdir build;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <CGAL/Combinatorial_map.h>
-#include "adjacency_incidence_cmap_utils.h"
-#include "lcc_inc_graph_utils.h"
 
 #include <iostream>
+#include "inc_graph_utils.h"
 
 typedef CGAL::Combinatorial_map<2> CM_2;
 typedef CM_2::Dart_handle Dart_handle;
@@ -12,11 +11,9 @@ int main(){
   Dart_handle dh1 = cm.create_dart();
   Dart_handle dh2 = cm.create_dart();
   Dart_handle dh3 = cm.create_dart();
-  Dart_handle dh4 = cm.create_dart();
   cm.sew<1>(dh1,dh2);
   cm.sew<1>(dh2,dh3);
-  cm.sew<1>(dh3,dh4);
-  cm.sew<1>(dh4,dh1);
-  write_inc<CM_2>(cm);
+  cm.sew<1>(dh3,dh1);
+  export_incidence_graph<CM_2,2>(cm, "test.csv");
   return 0;
 }
