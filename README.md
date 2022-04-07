467f727fc21b06945415c3e6123c7930add4024d
Michael Vennettilli <mikepvenn@gmail.com>
2022-04-07 16:34:43 +0200

    the plotting function works when done in pycharm, not the terminal. will fix this later

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 To do:
 
-1. Check over what I have for correctness.
+1. The code compiles fine in my PyCharm, but not from the terminal. This greatly decreases usability. Identify the source of the issue.
 
-2. Export the (dart, vertex) key-value pairs.
+2. Check over what I have for correctness.
 
-3. Import it into Python and plot the result.
-
-4. Write some proper documentation.
+3. Write some proper documentation.
--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include <CGAL/Linear_cell_complex_for_combinatorial_map.h>
-
+#include "shell_partitioner.h"
 #include "adjacency_incidence_cmap_utils.h"
 
 typedef CGAL::Linear_cell_complex_for_combinatorial_map<3> LCC_3;
 typedef LCC_3::Dart_handle Dart_handle;
 typedef LCC_3::Point Point;
 
+int main(int argc, char* argv[])
+{
+  double r_in = 1.0;
+  double r_out = 2.0;
+  int num_pts = 50;
+  if (argc>1){
+    num_pts = std::atoi(argv[1]);
+  }
+  if (argc>2){
+		r_in = std::atof(argv[2]);
+	}
+	if (argc>3){
+		r_out = std::atof(argv[3]);
+	}
 
-int main(){
-  LCC_3 lcc;
-  lcc.make_tetrahedron(Point(0,0,0),Point(1,0,0),Point(0,1,0),Point(0,0,1));
-  export_incidence_graph<LCC_3,lcc.dimension>(lcc, "test_inc.csv");
-  export_vertices<LCC_3>(lcc, "test_points.csv");
-  return 0;
+  std::vector<Point> points = random_spherical_points(num_pts);
+  LCC_3 shell = generate_shell(points, r_in, r_out);
+  lloyd_relaxation(shell, 10, r_in, r_out);
+  triangulate_all_faces(shell);
+  export_incidence_graph<LCC_3,shell.dimension>(shell, "test_inc.csv");
+  export_vertices<LCC_3>(shell, "test_points.csv");
+  return EXIT_SUCCESS;
 }
